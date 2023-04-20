618e4da62f66358e581d17ef5ce001cfad215ff1
RuebenBelwalkar <ruebenbelwalkar10@gmail.com>
2023-04-20 10:14:34 +0530

    first commit

---
+++ online-booking-system/online-booking-system/src/main/java/com/example/bookingSystem/OnlineBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookingSystemApplication.class, args);
+	}
+
+}
